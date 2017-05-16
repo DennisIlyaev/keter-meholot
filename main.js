6ecebe863bde4c4407920252ab89bb7c4466baf0
Dennis Ilyaev <DennisIlyaev@gmail.com>
2017-05-16 18:54:34 +0300

    update02

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
 
-  if (prompt('אנא הקש סיסמה') === 'keter') {
-    $('body').css('display', 'flex');
-  } else {
-    $('body').css('display', 'none');
-  }
+    if (prompt('אנא הקש סיסמה') === 'keter') {
+        $('body').css('display', 'flex');
+    } else {
+        $('body').css('display', 'none');
+    }
 
 
     document.onkeypress = function(e) {
@@ -32,20 +32,20 @@ $(document).ready(function() {
 
         if (confirm('להכניס תאריך?')) {
             var date = new Date(),
-              
+
                 year = date.getFullYear(),
                 month = date.getUTCMonth() + 1,
                 day = date.getUTCDate();
 
             if (day < 10) {
-              day = '0' + day;
+                day = '0' + day;
             }
 
             if (month < 10) {
-              month = '0' + month;
+                month = '0' + month;
             }
 
-            $(this).html(day + '/' +month + '/' + year);
+            $(this).html(day + '/' + month + '/' + year);
         } else {
 
             $(this).html('');
@@ -106,6 +106,46 @@ $(document).ready(function() {
 
         }
     });
+
+    $('table tr td:nth-child(3) div').on('keyup', function() {
+        if ($(this).html() == 'פאדי') {
+
+            $(this).parent().next().html('2611639');
+
+        } else if ($(this).html() == 'ולדימיר') {
+            $(this).parent().next().html('1601612');
+
+        } else if ($(this).html() == 'רבאח') {
+            $(this).parent().next().html('1095333');
+
+        } else if ($(this).html() == 'זכריה' || $(this).html() == 'זכר') {
+            $(this).parent().next().html('2175870');
+
+        } else if ($(this).html() == 'נור') {
+            $(this).parent().next().html('5682069');
+
+        } else if ($(this).html() == 'סרגיי') {
+            $(this).parent().next().html('2611439');
+
+        } else if ($(this).html() == 'ודים') {
+            $(this).parent().next().html('1534558');
+
+        } else if ($(this).html() == 'קובי') {
+            $(this).parent().next().html('8595652');
+
+
+
+
+        } else {
+            $(this).parent().next().html('').attr('contenteditable', 'true');
+        }
+    });
+
+
+
+
+
+
 
 
 });
