7c6e730cead3d06835578043155e0d1a709a1020
Dennis Ilyaev <DennisIlyaev@gmail.com>
2017-05-16 04:52:53 +0300

    update

--- main.js
+++ main.js
@@ -1,33 +1,111 @@
-// Map [Enter] key to work like the [Tab] key
-// Daniel P. Clark 2014
+$(document).ready(function() {
 
-// Catch the keydown for the entire document
-$(document).keydown(function(e) {
 
-  // Set self as the current item in focus
-  var self = $(':focus'),
-      // Set the form by the current item in focus
-      form = self.parents('form:eq(0)'),
-      focusable;
-
-  // Array of Indexable/Tab-able items
-  focusable = form.find('input,a,select,button,textarea,div[contenteditable=true]').filter(':visible');
-
-  function enterKey(){
-    if (e.which === 13 && !self.is('textarea,div[contenteditable=true]')) { // [Enter] key
-
-      // If not a regular hyperlink/button/textarea
-      if ($.inArray(self, focusable) && (!self.is('a,button'))){
-        // Then prevent the default [Enter] key behaviour from submitting the form
-        e.preventDefault();
-      } // Otherwise follow the link/button as by design, or put new line in textarea
-
-      // Focus on the next item (either previous or next depending on shift)
-      focusable.eq(focusable.index(self) + (e.shiftKey ? -1 : 1)).focus();
-
-      return false;
-    }
+  if (prompt('אנא הקש סיסמה') === 'keter') {
+    $('body').css('display', 'flex');
+  } else {
+    return false;
   }
-  // We need to capture the [Shift] key and check the [Enter] key either way.
-  if (e.shiftKey) { enterKey() } else { enterKey() }
+
+
+    document.onkeypress = function(e) {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+        }
+
+    };
+
+
+
+    $('div:gt(0)').focusin(function() {
+        $(this).css("background-color", "#FFFFCC");
+
+    });
+    $('div:gt(0)').focusout(function() {
+        $(this).css("background-color", "#FFFFFF");
+
+    });
+
+
+    $('table tr td:nth-child(2)', this).on('click', function() {
+
+
+        if (confirm('להכניס תאריך?')) {
+            var date = new Date(),
+              
+                year = date.getFullYear(),
+                month = date.getUTCMonth() + 1,
+                day = date.getUTCDate();
+
+            if (day < 10) {
+              day = '0' + day;
+            }
+
+            if (month < 10) {
+              month = '0' + month;
+            }
+
+            $(this).html(day + '/' +month + '/' + year);
+        } else {
+
+            $(this).html('');
+
+        }
+    });
+
+
+
+
+
+    $('table tr td:nth-child(7)', this).on('click', function() {
+
+
+        if (confirm('להכניס שעת כניסה?')) {
+            var date = new Date(),
+                minute = date.getMinutes(),
+                hour = date.getHours();
+
+            if (hour < 10) {
+                hour = '0' + hour;
+            }
+
+            if (minute < 10) {
+                minute = '0' + minute;
+            }
+
+
+            $(this).html(hour + ':' + minute);
+        } else {
+
+            $(this).html('');
+
+        }
+    });
+
+    $('table tr td:nth-child(8)', this).on('click', function() {
+
+
+        if (confirm('להכניס שעת יציאה?')) {
+            var date = new Date(),
+                minute = date.getMinutes(),
+                hour = date.getHours();
+
+            if (hour < 10) {
+                hour = '0' + hour;
+            }
+
+            if (minute < 10) {
+                minute = '0' + minute;
+            }
+
+
+            $(this).html(hour + ':' + minute);
+        } else {
+
+            $(this).html('');
+
+        }
+    });
+
+
 });
