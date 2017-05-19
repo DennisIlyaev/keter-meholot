6e1ff516a9311f51e0782d2fe7945ea92200c13e
Dennis Ilyaev <DennisIlyaev@gmail.com>
2017-05-19 13:10:26 +0300

    Asking premission to leave page

--- main.js
+++ main.js
@@ -8,12 +8,20 @@ $(document).ready(function() {
     }
 
 
+
+
     document.onkeypress = function(e) {
         if (e.keyCode === 13) {
             e.preventDefault();
         }
 
     };
+
+    window.onbeforeunload = confirmExit;
+
+    function confirmExit() {
+        return "You have attempted to leave this page.  If you have made any changes to the fields without clicking the Save button, your changes will be lost.  Are you sure you want to exit this page?";
+    }
 
 
 
